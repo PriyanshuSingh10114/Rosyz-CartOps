5bcbecb16aac48665ae8098e7f56ad61a9bf89cb
Priyanshu Singh <priyanshusingh22340@gmail.com>
2026-01-04 17:10:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
+set -e
 
-IMAGE_NAME="$DOCKER_REGISTRY_USERNAME/rosyz-cartops"
+IMAGE_NAME="priyanshusingh10114/rosyz-cartops:latest"
+CONTAINER_NAME="rosyz-cartops"
 
-echo "Logging in to Docker registry..."
-echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin
+echo "Fetching Docker Hub credentials from SSM..."
 
-echo "Pulling latest image..."
-docker pull $IMAGE_NAME:latest
+DOCKER_USER=$(aws ssm get-parameter \
+  --name /rosyz/docker/username \
+  --query Parameter.Value \
+  --output text)
 
-echo "Starting container..."
+DOCKER_PASS=$(aws ssm get-parameter \
+  --with-decryption \
+  --name /rosyz/docker/password \
+  --query Parameter.Value \
+  --output text)
+
+if [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
+  echo "Docker Hub credentials are missing"
+  exit 1
+fi
+
+echo "Logging in to Docker Hub..."
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+
+echo "Pulling image: $IMAGE_NAME"
+docker pull $IMAGE_NAME
+
+echo "Stopping old container if exists..."
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+echo "Starting new container..."
 docker run -d \
-  --name rosyz-cartops \
+  --name $CONTAINER_NAME \
   -p 80:80 \
-  $IMAGE_NAME:latest
-
+  $IMAGE_NAME
